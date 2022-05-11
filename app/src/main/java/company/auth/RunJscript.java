05790f89d3f0aba297fd1389022d1af833aeeafe
GTkatya <ekaterinaguryanova66@gmail.com>
2022-05-11 13:41:12 +0700

    save vers1

--- app/src/main/java/company/auth/RunJscript.java
+++ app/src/main/java/company/auth/RunJscript.java
@@ -9,6 +9,7 @@ import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RunJscript extends AppCompatActivity {
 
@@ -17,6 +18,7 @@ public class RunJscript extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run_jscript);
     }
+
     public void clickHandler(View view) {
         EditText input = findViewById(R.id.editTextInput);
         EditText output = findViewById(R.id.editTextOutput);
@@ -24,7 +26,13 @@ public class RunJscript extends AppCompatActivity {
         Context context = Context.enter(); //
         context.setOptimizationLevel(-1); // this is required[2]
         Scriptable scope = context.initStandardObjects();
-        Object result = context.evaluateString(scope, input.getText().toString(), "<cmd>", 1, null);
-        output.setText(result.toString());
+        try {
+            Object result = context.evaluateString(scope, input.getText().toString(), "<cmd>", 1, null);
+            output.setText(result.toString());
+        } catch (Exception e) {
+            Log.e("e", e.toString());
+            Toast.makeText(getApplicationContext(),
+                    "beast!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
