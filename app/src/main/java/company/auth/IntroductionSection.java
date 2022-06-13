4286075de552f6808cf5a32ccde263453bb4ef69
GTkatya <ekaterinaguryanova66@gmail.com>
2022-06-13 20:51:43 +0700

    add in activity "InproductionSection" two button and textView

--- app/src/main/java/company/auth/IntroductionSection.java
+++ app/src/main/java/company/auth/IntroductionSection.java
@@ -3,6 +3,8 @@ package company.auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class IntroductionSection extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class IntroductionSection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction_section);
-
+        final TextView txt_Is = findViewById(R.id.txt_Is);
+        IntroductionToJS1 IntroductionToJS1 = new IntroductionToJS1();
+        final Button b_out_Is = findViewById(R.id.b_out_Is);
+        final Button b_next_Is = findViewById(R.id.b_naxt_Is);
     }
 }
